d58711ff827cd59deb7a7d23674d5083a7ff0e09
Tiago Almeida <tyagow@hotmail.com.br>
2017-10-11 19:46:21 -0300

    refactor: code style refactor one property per line

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,7 +8,8 @@ function Book(props) {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ book.imageLinks.smallThumbnail }")` }}></div>
             <div className="book-shelf-changer">
-              <select value={book.shelf}  onChange={(event) => onMoveBook(book, event.target.value)}>
+              <select value={book.shelf}
+                      onChange={(event) => onMoveBook(book, event.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead" >Want to Read</option>
